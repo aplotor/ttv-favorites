9bd0f92f2b4717526e437ad11eb2840870da3df6
jc9108 <jc@grids.sh>
2023-12-29 02:26:57 -0500

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-export default { // https://vitejs.dev/config
+const vite_config = { // https://vitejs.dev/config
 	build: {
 		lib: {
 			formats: [
 				"es"
 			],
 			entry: {
-				"background": `./background/background.js`,
-				"foreground": `./foreground/foreground.js`,
-				"popup": `./popup/popup.js`
+				"background": "./background/background.js",
+				"foreground": "./foreground/foreground.js",
+				"popup": "./popup/popup.js"
 			},
 			fileName: (format, name) => `${name}.js`
 		},
 		outDir: "./build/" // vite empties this dir before building
 	}
 };
+
+export default vite_config;
